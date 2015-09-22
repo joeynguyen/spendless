bbb584ef1b643553d6db76a06a8aaab5d33686f7
Joey Nguyen <jnn193@gmail.com>
2015-09-22 18:41:08 -0500

    Added way to parse CSV file through input element.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-var parse = require('../../node_modules/csv-parse'),
-    fileReaderStream = require('../../node_modules/filereader-stream');
+var parse = require('../node_modules/csv-parse'),
+    fileReaderStream = require('../node_modules/filereader-stream');
 
 var holder = document.getElementById('holder');
 holder.ondragover = function () {
@@ -18,3 +18,12 @@ holder.ondrop = function (e) {
     rs.pipe(parser);
     return false;
 };
+
+function readFile() {
+    var selectedFile = document.getElementById('fileUpload').files[0],
+        rs = fileReaderStream(selectedFile),
+        parser = parse({columns: true, trim: true}, function(err, data){
+            console.log(JSON.stringify(data));
+        });
+    rs.pipe(parser);
+}
