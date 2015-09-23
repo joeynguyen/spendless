ac1536be8cf8c067e3d3418dbca23f5187a56425
Joey Nguyen <jnn193@gmail.com>
2015-09-23 01:58:07 -0500

    Added linvoDB database package and set up schema for storing data from parsed CSV files.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,12 @@
 var parse = require('../node_modules/csv-parse'),
-    fileReaderStream = require('../node_modules/filereader-stream');
+    fileReaderStream = require('../node_modules/filereader-stream'),
+    LinvoDB = require("linvodb3"),
+    modelName = "transactions",
+    schema = {transactionDate: Date, description: String, amount: Number},
+    options = {},
+    Transactions = new LinvoDB(modelName, schema, options);
+
+LinvoDB.dbPath = process.cwd();
 
 var holder = document.getElementById('holder');
 holder.ondragover = function () {
@@ -13,7 +20,22 @@ holder.ondrop = function (e) {
     var file = e.dataTransfer.files[0],
         rs = fileReaderStream(file),
         parser = parse({columns: true, trim: true}, function(err, data){
-            console.log(JSON.stringify(data));
+            var transactionsArray = Object.keys(data).map(function(k) { return data[k]; });
+            var filteredArray = [];
+            console.log("data length: " + transactionsArray.length);
+            console.log(transactionsArray);
+            for (var i = 0; i < transactionsArray.length; i++) {
+                var transactionObj = {};
+                transactionObj.transactionDate = transactionsArray[i]["Trans. Date"];
+                transactionObj.description = transactionsArray[i].Description;
+                transactionObj.amount = transactionsArray[i].Amount;
+                filteredArray.push(transactionObj);
+                console.log(filteredArray);
+            }
+            Transactions.insert(filteredArray, function(err, newDocs) {
+            });
+            // Transactions.insert([{"Description":"AIRBNB INC 415-800-5959 CAHGNYK7W"},{"Amount":"22.00"},{"Category":"Travel / Entertainment"}], function(err, newDocs) {
+            // });
         });
     rs.pipe(parser);
     return false;
