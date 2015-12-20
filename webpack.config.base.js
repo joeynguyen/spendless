65d72593f4d74b46c7bcd54780cf1afd0eff934f
Joey Nguyen <jnn193@gmail.com>
2015-12-20 15:51:34 -0600

    Removed unnecessary configs.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,11 +14,9 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    libraryTarget: 'commonjs2'
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
-    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
   },
   plugins: [
 
