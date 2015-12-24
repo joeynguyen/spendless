0768e571327935f71126d2bba5fb0c00e6a2a9c5
Joey Nguyen <jnn193@gmail.com>
2015-12-24 11:59:37 -0600

    Fixed indentation.

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,16 +5,16 @@ import TransactionsList from './TransactionsList.js';
 import styles from './Home.module.css';
 
 export default class Home extends Component {
-    render() {
-      return (
-        <div>
-          <div className={styles.container}>
-            <h2 className={styles.header}>Giao</h2>
-            <FileUpload />
-            <TransactionsList />
-            {/* <Link to="/counter">to Counter</Link> */}
-          </div>
+  render() {
+    return (
+      <div>
+        <div className={styles.container}>
+          <h2 className={styles.header}>Giao</h2>
+          <FileUpload />
+          <TransactionsList />
+          {/* <Link to="/counter">to Counter</Link> */}
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
