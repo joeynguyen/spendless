6cefbcded87752bab1018ced65a764da5ea8063f
Joey Nguyen <jnn193@gmail.com>
2015-12-24 01:47:04 -0600

    Added TransactionsList component.

---
+++ app/components/TransactionsList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class TransactionsList extends Component {
+  render() {
+    return (
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th>2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th>3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    );
+  }
+}
--- app/components/TransactionsTable.js
+++ app/components/TransactionsTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TransactionsList from './TransactionsList.js';
 
 export default class TransactionsTable extends Component {
   render() {
@@ -12,26 +13,7 @@ export default class TransactionsTable extends Component {
             <th>Amount</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th>2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th>3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
+        <TransactionsList />
       </table>
     );
   }
