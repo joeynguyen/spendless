53e08266fc1652a545cff204529f25b9c281b704
Joey Nguyen <jnn193@gmail.com>
2016-01-02 14:41:33 -0600

    Added modal window for future functionality of editing accounts.

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip, Modal, Button } from 'react-bootstrap';
 import styles from './Sidebar.module.css';
 import AccountGroup from './AccountGroup.js';
 
 export default class Home extends Component {
+  state = {
+    showModal: false
+  }
+  close = () => {
+    this.setState({ showModal: false });
+  }
+  open = () => {
+    this.setState({ showModal: true });
+  }
   render() {
-    const editTooltip = (<Tooltip>Edit</Tooltip>);
+    const editTooltip = (<Tooltip id="edit-tooltip">Edit</Tooltip>);
     return (
       <div id="sidebar" className={styles.sidebar + ' col-xs-3'}>
         <h3 className={styles['sidebar-header'] + ' clearfix'}>
           <span className="pull-left">Accounts</span>
           <span className="pull-right">
             <OverlayTrigger placement="top" overlay={editTooltip}>
-              <i id="accounts-edit" className={styles['accounts-edit'] + ' fa fa-fw fa-edit'}></i>
+              <i id="accounts-edit" onClick={this.open} className={styles['accounts-edit'] + ' fa fa-fw fa-edit'}></i>
             </OverlayTrigger>
           </span>
         </h3>
         <AccountGroup title="Banks" icon="bank" />
         <AccountGroup title="Credit Cards" icon="credit-card" />
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
