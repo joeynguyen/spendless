decffe7608bb8c209a33ec70f54f230b915698cf
Joey Nguyen <jnn193@gmail.com>
2016-01-02 14:09:15 -0600

    Added header and edit icon to sidebar.

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -6,6 +6,7 @@ export default class Home extends Component {
   render() {
     return (
       <div id="sidebar" className={styles.sidebar + ' col-xs-3'}>
+        <h3 className={styles['sidebar-header'] + ' clearfix'}><span className="pull-left">Accounts</span><span className="pull-right"><i id="accounts-edit" className={styles['accounts-edit'] + ' fa fa-fw fa-edit'}></i></span></h3>
         <AccountGroup title="Banks" icon="bank" />
         <AccountGroup title="Credit Cards" icon="credit-card" />
       </div>
