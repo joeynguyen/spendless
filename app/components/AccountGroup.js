092dd0c5365911284f66c6257a1a3e1041ca8189
Joey Nguyen <jnn193@gmail.com>
2016-01-02 03:05:17 -0600

    Created AccountGroup component for accounts in sidebar.

---
+++ app/components/AccountGroup.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+
+export default class AccountGroup extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  }
+  render() {
+    const panelHeader = (<div><i className={'fa fa-' + this.props.icon }></i> {this.props.title}</div>);
+    return (
+      <Panel collapsible defaultExpanded header={panelHeader}>
+        <ListGroup fill>
+          <ListGroupItem>Chase</ListGroupItem>
+          <ListGroupItem>Bank of America</ListGroupItem>
+          <ListGroupItem>&hellip;</ListGroupItem>
+        </ListGroup>
+      </Panel>
+    );
+  }
+}
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import styles from './Sidebar.module.css';
+import AccountGroup from './AccountGroup.js';
 
 export default class Home extends Component {
   render() {
     return (
       <div id="sidebar" className={styles.sidebar + ' col-xs-3'}>
-        <Panel collapsible defaultExpanded header="Panel heading">
-          Some default panel content here.
-          <ListGroup fill>
-            <ListGroupItem>Item 1</ListGroupItem>
-            <ListGroupItem>Item 2</ListGroupItem>
-            <ListGroupItem>&hellip;</ListGroupItem>
-          </ListGroup>
-          Some more panel content here.
-        </Panel>
+        <AccountGroup title="Banks" icon="bank" />
+        <AccountGroup title="Credit Cards" icon="credit-card" />
       </div>
     );
   }
