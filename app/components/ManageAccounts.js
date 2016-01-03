47bbb88038f7d6724dbd27d5f4e96e831a6a1ae1
Joey Nguyen <jnn193@gmail.com>
2016-01-02 23:00:11 -0600

    Added button for adding new accounts and updated layout of Manage Accounts window.

--- app/components/ManageAccounts.js
+++ app/components/ManageAccounts.js
@@ -13,11 +13,24 @@ export default class ManageAccountsWindow extends Component {
     return (
       <Modal show={this.props.showModal} onHide={this.closeWindow}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Manage Accounts</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Text in a modal</h4>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+          <div className="row">
+            <div className="col-xs-8">
+              <Button bsStyle="primary" bsSize="large" block><i className="fa fa-plus"></i> Add Account</Button>
+              <hr />
+              {/* Use state to display 'No accounts' */}
+              <p>No accounts found.</p>
+            </div>
+
+            <div className="col-xs-4">
+              <ul>
+                <li>1 Checking accounts</li>
+                <li>2 Credit card accounts</li>
+              </ul>
+            </div>
+          </div>
 
         </Modal.Body>
         <Modal.Footer>
