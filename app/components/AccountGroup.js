e4ddd7d42828de42e265ef997883c4e7511d4b0d
Joey Nguyen <jnn193@gmail.com>
2016-01-05 11:18:27 -0600

    Added functionality to retrieve user financial accounts from database.

--- app/components/AccountGroup.js
+++ app/components/AccountGroup.js
@@ -5,15 +5,18 @@ export default class AccountGroup extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
+    accounts: PropTypes.array.isRequired,
   }
   render() {
     const panelHeader = (<div><i className={'fa fa-lg fa-fw fa-' + this.props.icon }></i> {this.props.title}</div>);
     return (
       <Panel collapsible defaultExpanded header={panelHeader}>
         <ListGroup fill>
-          <ListGroupItem>Chase</ListGroupItem>
-          <ListGroupItem>Bank of America</ListGroupItem>
-          <ListGroupItem>&hellip;</ListGroupItem>
+          {
+            this.props.accounts.map(function(account) {
+              return <ListGroupItem key={account._id}>{account.name}</ListGroupItem>;
+            })
+          }
         </ListGroup>
       </Panel>
     );
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -4,9 +4,39 @@ import SidebarHeader from './SidebarHeader.js';
 import AccountGroup from './AccountGroup.js';
 import ManageAccountsWindow from './ManageAccounts.js';
 
+// PouchDB is loaded externally through a script tag in the browser
+const db = new PouchDB('accounts');
+
 export default class Home extends Component {
   state = {
-    showModal: false
+    showModal: false,
+    accounts: [],
+  }
+  componentDidMount() {
+    this.importAccounts();
+  }
+  // Show the current list of transactions by reading them from the database
+  importAccounts = () => {
+    db.allDocs({
+      include_docs: true,
+      descending: true,
+    }).then((result) => {
+      const allAccounts = result.rows.map(function(row) {
+        return {
+          '_id': row.doc._id,
+          'name': row.doc.name,
+          'type': row.doc.type,
+          'bank': row.doc.bank,
+          'cc': row.doc.cc,
+        };
+      });
+      console.log('allAccounts: ', allAccounts);
+      this.setState({
+        accounts: this.state.accounts.concat(allAccounts)
+      });
+    }).catch(function(err) {
+      console.log(err);
+    });
   }
   close = () => {
     this.setState({ showModal: false });
@@ -15,11 +45,19 @@ export default class Home extends Component {
     this.setState({ showModal: true });
   }
   render() {
+    const ccAccounts = this.state.accounts.filter(function(account) {
+      return account.type === 'creditcard';
+    });
+    const bankAccounts = this.state.accounts.filter(function(account) {
+      return account.type === 'bank';
+    });
+    console.log('credit cards: ', ccAccounts);
+    console.log('banks: ', bankAccounts);
     return (
       <div id="sidebar" className={styles.sidebar + ' col-xs-3'}>
         <SidebarHeader open={this.open} />
-        <AccountGroup title="Banks" icon="bank" />
-        <AccountGroup title="Credit Cards" icon="credit-card" />
+        <AccountGroup title="Banks" icon="bank" accounts={bankAccounts} />
+        <AccountGroup title="Credit Cards" icon="credit-card" accounts={ccAccounts} />
         <ManageAccountsWindow showModal={this.state.showModal} close={this.close} />
       </div>
     );
