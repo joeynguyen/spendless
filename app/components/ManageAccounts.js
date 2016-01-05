eaa8f4bc8c546ce068958a82cc884000c9bfd62c
Joey Nguyen <jnn193@gmail.com>
2016-01-04 22:07:34 -0600

    Added expandable panel to open/close when toggling Add Account button.

--- app/components/ManageAccounts.js
+++ app/components/ManageAccounts.js
@@ -1,15 +1,31 @@
 import React, { Component, PropTypes } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Panel } from 'react-bootstrap';
+
+// PouchDB is loaded externally through a script tag in the browser
+const db = new PouchDB('accounts');
 
 export default class ManageAccountsWindow extends Component {
   static propTypes = {
     showModal: PropTypes.bool.isRequired,
     close: PropTypes.func.isRequired,
   }
+  state = {
+    showAdd: false
+  }
+  toggleAddAccount = () => {
+    this.setState({showAdd: !this.state.showAdd});
+  }
   closeWindow = () => {
+    this.setState({showAdd: false});
     this.props.close();
   }
   render() {
+    let addButton = {};
+    if (this.state.showAdd) {
+      addButton = { style: 'danger', class: '', text: 'Cancel' };
+    } else {
+      addButton = { style: 'primary', class: 'fa fa-plus', text: ' Add Account' };
+    }
     return (
       <Modal show={this.props.showModal} onHide={this.closeWindow}>
         <Modal.Header closeButton>
@@ -18,7 +34,11 @@ export default class ManageAccountsWindow extends Component {
         <Modal.Body>
           <div className="row">
             <div className="col-xs-8">
-              <Button bsStyle="primary" bsSize="large" block><i className="fa fa-plus"></i> Add Account</Button>
+              <Button onClick={this.toggleAddAccount} bsStyle={addButton.style} bsSize="large" block><i className={addButton.class}></i>{addButton.text}</Button>
+              <Panel collapsible expanded={this.state.showAdd}>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+              </Panel>
               <hr />
               {/* Use state to display 'No accounts' */}
               <p>No accounts found.</p>
