e8b7a2671f926fd5bdea1c2754d6d345bab6a7e7
Joey Nguyen <jnn193@gmail.com>
2016-01-05 02:00:03 -0600

    Added form for adding new accounts.

--- app/components/ManageAccounts.js
+++ app/components/ManageAccounts.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Modal, Button, Panel } from 'react-bootstrap';
+import { Modal, Button, ButtonInput, Collapse, Panel, Input } from 'react-bootstrap';
 
 // PouchDB is loaded externally through a script tag in the browser
 const db = new PouchDB('accounts');
@@ -10,14 +10,25 @@ export default class ManageAccountsWindow extends Component {
     close: PropTypes.func.isRequired,
   }
   state = {
-    showAdd: false
+    showAdd: false,
+    showBank: false,
+    showCredit: false
   }
   toggleAddAccount = () => {
     this.setState({showAdd: !this.state.showAdd});
   }
   closeWindow = () => {
-    this.setState({showAdd: false});
+    this.setState({showAdd: false, showBank: false, showCredit: false});
     this.props.close();
+  }
+  handleType = (event) => {
+    if (event.target.value === 'creditcard') {
+      this.setState({showCredit: true, showBank: false});
+    } else if (event.target.value === 'bank') {
+      this.setState({showBank: true, showCredit: false});
+    } else {
+      this.setState({showCredit: false, showBank: false});
+    }
   }
   render() {
     let addButton = {};
@@ -36,8 +47,35 @@ export default class ManageAccountsWindow extends Component {
             <div className="col-xs-8">
               <Button onClick={this.toggleAddAccount} bsStyle={addButton.style} bsSize="large" block><i className={addButton.class}></i>{addButton.text}</Button>
               <Panel collapsible expanded={this.state.showAdd}>
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                {/* Add form validation. Don't allow 'select' value to be chosen */}
+                <form>
+                  <Input type="text" label="Name" placeholder="Enter a name for the account" />
+                  <Input type="select" label="Type" placeholder="Type" onChange={this.handleType} >
+                    <option value="select">select</option>
+                    <option value="bank">Bank</option>
+                    <option value="creditcard">Credit Card</option>
+                  </Input>
+
+                  <Collapse in={this.state.showBank}>
+                    <div>
+                      <Input type="text" label="Name of Institution" placeholder="Enter the name of the financial institution" />
+                    </div>
+                  </Collapse>
+                  <Collapse in={this.state.showCredit}>
+                    <div>
+                      <Input type="select" label="Credit Card Company" placeholder="Credit Card Company">
+                        <option value="select">select</option>
+                        <option value="visa">Visa</option>
+                        <option value="mc">Mastercard</option>
+                        <option value="amex">American Express</option>
+                        <option value="discover">Discover</option>
+                        <option value="diners">Diners Club</option>
+                      </Input>
+                    </div>
+                  </Collapse>
+
+                  <ButtonInput bsStyle="primary" type="submit" value="Add" />
+                </form>
               </Panel>
               <hr />
               {/* Use state to display 'No accounts' */}
