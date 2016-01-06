174714ec30b2c14b53d59c526971d1ad2c27bda3
Joey Nguyen <jnn193@gmail.com>
2016-01-06 14:57:39 -0600

    Updated routing to display the correct account name and financial institution on each account page.

--- app/containers/AccountPage.js
+++ app/containers/AccountPage.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import FileUpload from '../components/FileUpload.js';
 import TransactionsList from '../components/TransactionsList.js';
 import styles from '../components/Account.module.css';
 import { Link } from 'react-router';
 
 // PouchDB is loaded externally through a script tag in the browser
-const db = new PouchDB('transactions');
-db.info().then(function(info) {
-  console.log('db info: ', info);
+const transDB = new PouchDB('transactions');
+transDB.info().then(function(info) {
+  console.log('transDB info: ', info);
 });
 
+const acctsDB = new PouchDB('accounts');
+
 export default class Account extends Component {
+  static propTypes = {
+    params: PropTypes.object.isRequired
+  }
+
   state = {
+    account: {},
     transactions: []
   }
 
   componentDidMount() {
+    this.importAccount();
     this.importTransactions();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.importAccount(nextProps);
   }
 
   logState = () => {
@@ -38,7 +50,7 @@ export default class Account extends Component {
     console.log(this.state.transactions);
     this.state.transactions.forEach(function(transaction) {
       console.log(transaction);
-      db.put(transaction).then(function(result) {
+      transDB.put(transaction).then(function(result) {
         console.log('Successfully posted transactions');
         console.log(result);
       }).catch(function(err) {
@@ -49,7 +61,7 @@ export default class Account extends Component {
 
   // Show the current list of transactions by reading them from the database
   importTransactions = () => {
-    db.allDocs({
+    transDB.allDocs({
       include_docs: true,
       descending: true,
     }).then((result) => {
@@ -71,20 +83,33 @@ export default class Account extends Component {
       console.log(err);
     });
   }
+  importAccount = (_props) => {
+    let props = this.props;
+    if (_props) {
+      props = _props;
+    }
+    acctsDB.get(props.params.id).then((doc) => {
+      this.setState({ account: doc });
+      console.log(doc);
+    });
+  }
   render() {
-    return (
-      <div className="col-xs-9">
-        <div className="header">
-          <h3 className={styles.header}>Discover Card <br />
-            <small>Discover IT</small></h3>
+    if (this.state.account) {
+      return (
+        <div className="col-xs-9">
+          <div className="header">
+            <h3 className={styles.header}>{this.state.account.name} <br />
+              <small>{this.state.account.bank || this.state.account.cc}</small></h3>
+          </div>
+          <FileUpload onUpdate={this.onUpdate} />
+          <button onClick={this.handleSave} >Save</button>
+          <p><Link to="/">Back to Home</Link></p>
+          <TransactionsList transactions={this.state.transactions} />
+          {/* <Link to="/counter">to Counter</Link> */}
+          <button onClick={this.logState}>console.log(state)</button>
         </div>
-        <FileUpload onUpdate={this.onUpdate} />
-        <button onClick={this.handleSave} >Save</button>
-        <p><Link to="/">Back to Home</Link></p>
-        <TransactionsList transactions={this.state.transactions} />
-        {/* <Link to="/counter">to Counter</Link> */}
-        <button onClick={this.logState}>console.log(state)</button>
-      </div>
-    );
+      );
+    }
+    return <div>Loading...</div>;
   }
 }
