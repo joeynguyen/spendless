323441c82d4a44a9cd3d4c193df7b75bef23f4c8
Joey Nguyen <jnn193@gmail.com>
2016-01-05 22:02:56 -0600

    Created new route for account pages.

---
+++ app/containers/AccountPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Account from '../components/Account';
+
+export default class AccountPage extends Component {
+  render() {
+    return (
+      <Account />
+    );
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
+import AccountPage from './containers/AccountPage';
 // import CounterPage from './containers/CounterPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="account/:id" component={AccountPage} />
     {/* <Route path="/counter" component={CounterPage} /> */}
   </Route>
 );
