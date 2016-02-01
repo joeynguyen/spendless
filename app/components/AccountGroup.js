73a16c0ce9df117c559c4219f8ea62037c8800b0
Joey Nguyen <jnn193@gmail.com>
2016-02-01 00:42:03 -0600

    Converted AccountGroup from React class to functional stateless component.

--- app/components/AccountGroup.js
+++ app/components/AccountGroup.js
@@ -1,29 +1,34 @@
-import React, { Component, PropTypes } from 'react';
+import React, {  PropTypes } from 'react';
 import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-export default class AccountGroup extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    accounts: PropTypes.array.isRequired,
-  }
-  render() {
-    const panelHeader = (<div><i className={'fa fa-lg fa-fw fa-' + this.props.icon }></i> {this.props.title}</div>);
-    return (
-      <Panel collapsible defaultExpanded header={panelHeader}>
-        <ListGroup fill>
-          {
-            this.props.accounts.map(function(account) {
-              return (
-                <LinkContainer to={'/account/' + account._id} key={account._id}>
-                  <ListGroupItem>{account.name}</ListGroupItem>
-                </LinkContainer>
-              );
-            })
-          }
-        </ListGroup>
-      </Panel>
-    );
-  }
-}
+const AccountGroup = ({ title, icon, accounts }) => {
+  const panelHeader = (
+    <div>
+      <i className={'fa fa-lg fa-fw fa-' + icon }></i>
+      {` ${title}`}
+    </div>
+  );
+  return (
+    <Panel collapsible defaultExpanded header={panelHeader}>
+      <ListGroup fill>
+        {
+          accounts.map(function(account) {
+            return (
+              <LinkContainer to={'/account/' + account._id} key={account._id}>
+                <ListGroupItem>{account.name}</ListGroupItem>
+              </LinkContainer>
+            );
+          })
+        }
+      </ListGroup>
+    </Panel>
+  );
+};
+AccountGroup.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  accounts: PropTypes.array.isRequired,
+};
+
+export default AccountGroup;
