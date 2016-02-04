2a2aaeeaa4c30ae3035331866d23a15a4e41c1ba
Joey Nguyen <jnn193@gmail.com>
2016-02-04 02:39:35 -0600

    Created AccountContainer component to handle Redux actions and state.

---
+++ src/account/AccountContainer.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import AccountGroup from './AccountGroup.js';
+import { fetchAccounts } from './AccountActions.js';
+
+class AccountContainer extends Component {
+  static propTypes = {
+    doFetchAccounts: PropTypes.func.isRequired,
+    accounts: PropTypes.array
+  }
+
+  componentDidMount() {
+    this.props.doFetchAccounts();
+  }
+
+  render() {
+    if (!this.props.accounts) {
+      return <div>Loading...</div>;
+    }
+    const ccAccounts = this.props.accounts.filter((account) => {
+      return account.type === 'creditcard';
+    });
+    const bankAccounts = this.props.accounts.filter((account) => {
+      return account.type === 'bank';
+    });
+    return (
+      <div>
+        <AccountGroup title="Banks" icon="bank" accounts={bankAccounts} />
+        <AccountGroup title="Credit Cards" icon="credit-card" accounts={ccAccounts} />
+      </div>
+    );
+  }
+}
+
+// Anything returned from this function will end up as props
+// on the AccountContainer container
+function mapStateToProps(state) {
+  return {
+    // state comes from the <Provider>'s 'store' property in index.js
+    accounts: state.accounts
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ doFetchAccounts: fetchAccounts }, dispatch);
+}
+
+// Promote AccountContainer from a Component to a Container, a
+// component that is aware of the state that's contained by Redux
+// 'connect' takes a Function and a Component and produces a Container
+// arguments are passed into the Container as props(?)
+// https://github.com/rackt/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
+export default connect(mapStateToProps, mapDispatchToProps)(AccountContainer);
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,15 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from 'react';
 import styles from './Sidebar.module.css';
 import SidebarHeader from './SidebarHeader.js';
-import AccountGroup from '../account/AccountGroup.js';
-import { fetchAccounts } from '../account/AccountActions.js';
 import ManageAccountsWindow from './ManageAccounts.js';
+import AccountContainer from '../account/AccountContainer.js';
 
 // PouchDB is loaded externally through a script tag in the browser
 // const db = new PouchDB('accounts');
 
-class Sidebar extends Component {
-  static propTypes = {
-    doFetchAccounts: PropTypes.func.isRequired,
-    accounts: PropTypes.array
-  }
-
+export default class Sidebar extends Component {
   state = {
     showModal: false,
-  }
-  componentDidMount() {
-    this.props.doFetchAccounts();
   }
   close = () => {
     this.setState({ showModal: false });
@@ -29,42 +18,13 @@ class Sidebar extends Component {
     this.setState({ showModal: true });
   }
   render() {
-    if (!this.props.accounts) {
-      return <div>Loading...</div>;
-    }
-    const ccAccounts = this.props.accounts.filter((account) => {
-      return account.type === 'creditcard';
-    });
-    const bankAccounts = this.props.accounts.filter((account) => {
-      return account.type === 'bank';
-    });
     return (
       <div id="sidebar" className={styles.sidebar + ' col-xs-3 sidebar'}>
         <SidebarHeader open={this.open} />
-        <AccountGroup title="Banks" icon="bank" accounts={bankAccounts} />
-        <AccountGroup title="Credit Cards" icon="credit-card" accounts={ccAccounts} />
+        <AccountContainer />
         <ManageAccountsWindow showModal={this.state.showModal} close={this.close} />
       </div>
     );
   }
 }
 
-// Anything returned from this function will end up as props
-// on the Sidebar container
-function mapStateToProps(state) {
-  return {
-    // state comes from the <Provider>'s 'store' property in index.js
-    accounts: state.accounts
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ doFetchAccounts: fetchAccounts }, dispatch);
-}
-
-// Promote Sidebar from a Component to a Container, a
-// component that is aware of the state that's contained by Redux
-// 'connect' takes a Function and a Component and produces a Container
-// arguments are passed into the Container as props(?)
-// https://github.com/rackt/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
