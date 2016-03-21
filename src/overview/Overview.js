96c117c9884effd1f10528abec94473e2125a743
Joey Nguyen <jnn193@gmail.com>
2016-03-21 03:26:43 -0500

    Updated placeholder text on Overview.

--- src/overview/Overview.js
+++ src/overview/Overview.js
@@ -5,7 +5,8 @@ const Overview = () => {
     <div className="col-xs-9">
       <div className="header">
         <h3>Accounts Summary</h3>
-        <p>Select an account from the sidebar to see its details.</p>
+        <p>In the future, this page will display charts with data from your accounts.</p>
+        <p>For now, select an account from the sidebar to see its details.</p>
       </div>
     </div>
   );
