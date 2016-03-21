14ad4bec3f5dc5523e30aea209ed13dca10f5b00
Joey Nguyen <jnn193@gmail.com>
2016-03-20 23:38:18 -0500

    Created AppContainer to listen for PouchDB changes.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './app/App.js';
+import AppContainer from './app/AppContainer.js';
 import Overview from './overview/Overview.js';
 import AccountDetail from './account/AccountDetail.js';
 import HelpContent from './help/HelpContent.js';
 
 export default (
-  <Route path="/" component={App}>
+  <Route path="/" component={AppContainer}>
     <IndexRoute component={Overview} />
     <Route path="help" component={HelpContent} />
     <Route path="account/:id" component={AccountDetail} />
