608339c44fe4482e218e1a7949702bda416b5437
Joey Nguyen <jnn193@gmail.com>
2016-03-22 00:02:55 -0500

    Moved functions for AccountsListItem out of render method.

--- src/manage-accounts/AccountsListItem.js
+++ src/manage-accounts/AccountsListItem.js
@@ -18,11 +18,43 @@ class AccountsListItem extends Component {
     confirmDeleteVisible: false,
     confirmDeleteText: '',
   }
-  handleAlertDismiss = () => {
-    this.setState({alertVisible: false});
+  toggleSettings = () => {
+    if (this.state.settingsVisible) {
+      this.props.resetForm();
+    }
+    this.setState({ settingsVisible: !this.state.settingsVisible });
   }
-  handleAlertShow = () => {
-    this.setState({alertVisible: true});
+  toggleConfirmDelete = () => {
+    this.setState({ confirmDeleteVisible: !this.state.confirmDeleteVisible });
+  }
+  handleConfirmDeleteText = (e) => {
+    this.setState({ confirmDeleteText: e.target.value });
+  }
+  handleUpdateAccount = (accountToUpdate) => {
+    const self = this;
+    const newAccountObj = Object.assign({}, accountToUpdate, {
+      name: this.props.fields.accountName.value,
+      type: this.props.fields.accountType.value,
+      company: this.props.fields.accountCompany.value,
+    });
+    // Update account in DB
+    db.put(newAccountObj).then(function(result) {
+      console.log('Successfully updated account', result);
+      self.setState({alertVisible: true});
+    }).catch(function(err) {
+      console.log(err);
+      // TODO: Add error message after update fail
+    });
+  }
+  handleDeleteAccount = (accountToDelete) => {
+    // Remove account from DB
+    db.remove(accountToDelete).then(function(result) {
+      console.log('Successfully deleted account', result);
+      // TODO: Add success message after successful delete
+    }).catch(function(err) {
+      console.log('Error trying to delete account', err);
+      // TODO: Add error message after delete fail
+    });
   }
 
   render() {
@@ -31,53 +63,14 @@ class AccountsListItem extends Component {
         cursor: 'pointer',
       }
     };
-    const { fields: { accountName, accountType, accountCompany }, resetForm } = this.props;
-
-    const toggleSettings = () => {
-      if (this.state.settingsVisible) {
-        resetForm();
-      }
-      this.setState({ settingsVisible: !this.state.settingsVisible });
-    };
-    const toggleConfirmDelete = () => {
-      this.setState({ confirmDeleteVisible: !this.state.confirmDeleteVisible });
-    };
-    const handleConfirmDeleteText = (e) => {
-      this.setState({ confirmDeleteText: e.target.value });
-    };
-    const handleUpdateAccount = (accountToUpdate) => {
-      const self = this;
-      const newAccountObj = Object.assign({}, accountToUpdate, {
-        name: this.props.fields.accountName.value,
-        type: this.props.fields.accountType.value,
-        company: this.props.fields.accountCompany.value,
-      });
-      // Update account in DB
-      db.put(newAccountObj).then(function(result) {
-        console.log('Successfully updated account', result);
-        self.handleAlertShow();
-      }).catch(function(err) {
-        console.log(err);
-        // TODO: Add error message after update fail
-      });
-    };
-    const handleDeleteAccount = (accountToDelete) => {
-      // Remove account from DB
-      db.remove(accountToDelete).then(function(result) {
-        console.log('Successfully deleted account', result);
-        // TODO: Add success message after successful delete
-      }).catch(function(err) {
-        console.log('Error trying to delete account', err);
-        // TODO: Add error message after delete fail
-      });
-    };
+    const { fields: { accountName, accountType, accountCompany } } = this.props;
     let alertMessage;
 
     if (this.state.alertVisible) {
       alertMessage = (
         <div>
           <br />
-          <Alert bsStyle="success" onDismiss={this.handleAlertDismiss} dismissAfter={2000}>
+          <Alert bsStyle="success" onDismiss={() => this.setState({alertVisible: false})} dismissAfter={2000}>
             <p>Account updated successfully!</p>
           </Alert>
         </div>
@@ -86,7 +79,7 @@ class AccountsListItem extends Component {
 
     return (
       <Well bsSize="small" key={this.props.account._id}>
-        <i className="fa fa-lg fa-fw fa-cog pull-right" style={styles.removeIcon} onClick={toggleSettings} ></i>
+        <i className="fa fa-lg fa-fw fa-cog pull-right" style={styles.removeIcon} onClick={this.toggleSettings} ></i>
         <h4>{this.props.account.name}</h4>
         <p>{this.props.account.type === 'bank' ? 'Bank' : 'Credit Card'} - {this.props.account.company}</p>
         <Collapse in={this.state.settingsVisible}>
@@ -135,17 +128,17 @@ class AccountsListItem extends Component {
               </Collapse>
               <Button
                 disabled={this.props.pristine}
-                onClick={() => handleUpdateAccount(this.props.account)}
+                onClick={() => this.handleUpdateAccount(this.props.account)}
                 bsStyle="success"
               >Update</Button>
               {' '}
               <Button
-                onClick={toggleSettings}
+                onClick={this.toggleSettings}
               >Cancel</Button>
               <Button
                 className="pull-right"
                 disabled={this.state.confirmDeleteVisible}
-                onClick={toggleConfirmDelete}
+                onClick={this.toggleConfirmDelete}
                 bsStyle="danger"
               >Delete</Button>
               <div className="clearfix"></div>
@@ -158,18 +151,18 @@ class AccountsListItem extends Component {
                       <Input
                         type="text"
                         value={this.state.confirmDeleteText}
-                        onChange={handleConfirmDeleteText}
+                        onChange={this.handleConfirmDeleteText}
                         placeholder="DELETE" />
                     </div>
                     <div className="col-xs-6">
                       <Button
                         disabled={this.state.confirmDeleteText !== 'DELETE'}
-                        onClick={() => handleDeleteAccount(this.props.account)}
+                        onClick={() => this.handleDeleteAccount(this.props.account)}
                         bsStyle="success"
                       >Confirm</Button>
                       {' '}
                       <Button
-                        onClick={toggleConfirmDelete}
+                        onClick={this.toggleConfirmDelete}
                       >Cancel</Button>
                     </div>
                   </div>
