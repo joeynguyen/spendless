d0978f2df7f7f4230568448d2c0f286ed9b3bb81
Joey Nguyen <jnn193@gmail.com>
2016-03-28 21:05:54 -0500

    Moved alertMessage and DeleteAccountFormContainer outside of edit account form.

--- src/manage-accounts/EditAccountFormContainer.js
+++ src/manage-accounts/EditAccountFormContainer.js
@@ -60,63 +60,65 @@ class EditAccountFormContainer extends Component {
       );
     }
     return (
-      <form>
-        <Input
-          type="text"
-          label="Name"
-          {...accountName}
-        />
-        <Input
-          type="select"
-          label="Type"
-          {...accountType}
-        >
-          <option value="">select...</option>
-          <option value="bank">Bank</option>
-          <option value="creditcard">Credit Card</option>
-        </Input>
-        <Collapse in={accountType.value === 'bank'}>
-          <div>
-            <Input
-              type="text"
-              label="Name of Institution"
-              {...accountCompany}
-            />
-          </div>
-        </Collapse>
-        <Collapse in={accountType.value === 'creditcard'}>
-          <div>
-            <Input
-              type="select"
-              label="Credit Card Company"
-              {...accountCompany}
-            >
-              <option value="">select...</option>
-              <option value="Visa">Visa</option>
-              <option value="MasterCard">Mastercard</option>
-              <option value="American Express">American Express</option>
-              <option value="Discover">Discover</option>
-              <option value="Diners Club">Diners Club</option>
-              <option value="JCB">JCB</option>
-              <option value="Other">Other</option>
-            </Input>
-          </div>
-        </Collapse>
-        <Button
-          disabled={this.props.pristine}
-          onClick={() => this.handleUpdateAccount(this.props.account)}
-          bsStyle="success"
-        >Update</Button>
-        {' '}
-        <Button
-          onClick={this.props.toggleSettings}
-        >Cancel</Button>
-        <Button
-          className="pull-right"
-          disabled={this.state.confirmDeleteVisible}
-          onClick={this.toggleConfirmDelete}
-          bsStyle="danger"
-        >Delete</Button>
+      <div>
+        <form>
+          <Input
+            type="text"
+            label="Name"
+            {...accountName}
+          />
+          <Input
+            type="select"
+            label="Type"
+            {...accountType}
+          >
+            <option value="">select...</option>
+            <option value="bank">Bank</option>
+            <option value="creditcard">Credit Card</option>
+          </Input>
+          <Collapse in={accountType.value === 'bank'}>
+            <div>
+              <Input
+                type="text"
+                label="Name of Institution"
+                {...accountCompany}
+              />
+            </div>
+          </Collapse>
+          <Collapse in={accountType.value === 'creditcard'}>
+            <div>
+              <Input
+                type="select"
+                label="Credit Card Company"
+                {...accountCompany}
+              >
+                <option value="">select...</option>
+                <option value="Visa">Visa</option>
+                <option value="MasterCard">Mastercard</option>
+                <option value="American Express">American Express</option>
+                <option value="Discover">Discover</option>
+                <option value="Diners Club">Diners Club</option>
+                <option value="JCB">JCB</option>
+                <option value="Other">Other</option>
+              </Input>
+            </div>
+          </Collapse>
+          <Button
+            disabled={this.props.pristine}
+            onClick={() => this.handleUpdateAccount(this.props.account)}
+            bsStyle="success"
+          >Update</Button>
+          {' '}
+          <Button
+            onClick={this.props.toggleSettings}
+          >Cancel</Button>
+          <Button
+            className="pull-right"
+            disabled={this.state.confirmDeleteVisible}
+            onClick={this.toggleConfirmDelete}
+            bsStyle="danger"
+          >Delete</Button>
+        </form>
         { alertMessage }
         <Collapse in={this.state.confirmDeleteVisible}>
           <div>
@@ -126,7 +128,7 @@ class EditAccountFormContainer extends Component {
             />
           </div>
         </Collapse>
-      </form>
+      </div>
     );
   }
 }
