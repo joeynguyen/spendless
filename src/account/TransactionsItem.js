d2d5dbbfb847a533aa595fe06e7ba8bc6323d2ce
Joey Nguyen <jnn193@gmail.com>
2016-04-08 18:15:46 -0500

    Added tooltip to show a transaction's notes content on hover.

--- src/account/TransactionsItem.js
+++ src/account/TransactionsItem.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import moment from 'moment';
 
 const TransactionsItem = ({ transaction, unsaved, doToggleEditTransaction, doSelectActiveTransaction }) => {
@@ -15,7 +16,11 @@ const TransactionsItem = ({ transaction, unsaved, doToggleEditTransaction, doSel
   }
   let note;
   if (transaction.notes !== '') {
-    note = (<i className="fa fa-fw fa-lg fa-sticky-note-o" ></i>);
+    note = (
+      <OverlayTrigger placement="left" overlay={<Tooltip id={transaction._id}>{transaction.notes}</Tooltip>}>
+        <i className="fa fa-fw fa-lg fa-sticky-note-o" ></i>
+      </OverlayTrigger>
+    );
   }
   return (
     <tr style={rowStyle}>
