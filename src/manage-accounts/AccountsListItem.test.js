d78e19120d8d6ee2d330779156ebffcf40c386e7
Joey Nguyen <jnn193@gmail.com>
2016-10-23 14:32:40 -0500

    Added tests for testing component state to AccountsListItem

--- src/manage-accounts/AccountsListItem.test.js
+++ src/manage-accounts/AccountsListItem.test.js
@@ -2,6 +2,8 @@ import expect from 'expect';
 import React from 'react';
 import { shallow } from 'enzyme';
 import AccountsListItem from './AccountsListItem';
+import EditAccountFormContainer from './EditAccountFormContainer';
+
 
 const mockAccount = (type = 'creditcard') => {
   return {
@@ -13,31 +15,45 @@ const mockAccount = (type = 'creditcard') => {
   };
 };
 
-function setup(account) {
+function shallowSetup(account) {
   return shallow(<AccountsListItem account={account} />);
 }
 
 describe('AccountsListItem', () => {
   describe('renders correct elements by default', () => {
-    const wrapper = setup(mockAccount());
+    const wrapper = shallowSetup(mockAccount());
 
-    it('is wrapped in a Well component', () => {
-      expect(wrapper.find('Well').length).toBe(1);
-    });
     it('renders the account name', () => {
       expect(wrapper.find('.account-name').text()).toBe('Chase Freedom');
     });
     it('renders a cog icon element', () => {
       expect(wrapper.find('.fa-cog').length).toBe(1);
     });
-    it('renders the company name with type as Credit Card by default', () => {
+    it('renders the company info with type as Credit Card by default', () => {
       expect(wrapper.find('.company-info').text()).toBe('Credit Card - MasterCard');
     });
   });
+
   describe('renders correct elements based on specific behaviors', () => {
-    it('renders the company name with type as Bank if type is "bank"', () => {
-      const wrapper = setup(mockAccount('bank'));
+    it('renders the company info with Bank if type is "bank"', () => {
+      const wrapper = shallowSetup(mockAccount('bank'));
       expect(wrapper.find('.company-info').text()).toBe('Bank - MasterCard');
+    });
+  });
+
+  describe('it has correct component state based on behavior', () => {
+    const wrapper = shallowSetup(mockAccount());
+    it('has settingsVisible as false by default', () => {
+      expect(wrapper.state().settingsVisible).toBe(false);
+    });
+    it('doesn\'t show EditAccountFormContainer by default', () => {
+      expect(wrapper.find(EditAccountFormContainer).length).toBe(0);
+    });
+    it('when cog is clicked, settingsVisible state is true and EditAccountFormContainer is rendered', () => {
+      const cogIcon = wrapper.find('.fa-cog');
+      cogIcon.simulate('click');
+      expect(wrapper.state().settingsVisible).toBe(true);
+      expect(wrapper.find(EditAccountFormContainer).length).toBe(1);
     });
   });
 });
