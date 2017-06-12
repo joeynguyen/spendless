9115230541c4baf778ea6346bd18be7511636f80
Joey Nguyen <jnn193@gmail.com>
2017-06-12 13:55:44 -0500

    Fix hot reloading not updating on changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
 import Root from './app/Root.jsx';
@@ -7,19 +7,21 @@ import { configureStore, history } from './store/configureStore.js';
 
 const store = configureStore();
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component store={store} history={history} />
-    </AppContainer>,
-    document.getElementById('root')
-  );
-};
-
-render(Root);
+render(
+  <AppContainer>
+    <Root store={store} history={history} />
+  </AppContainer>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
-  module.hot.accept('./app/Root.jsx', () => {
-    render(Root);
+  module.hot.accept('./app/Root', () => {
+    const NextRoot = require('./app/Root'); // eslint-disable-line global-require
+    render(
+      <AppContainer>
+        <NextRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('root')
+    );
   });
 }
