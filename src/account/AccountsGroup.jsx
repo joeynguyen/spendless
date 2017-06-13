6b3a74a67378e5490f47a6cb74ead06ddcca54ae
Joey Nguyen <jnn193@gmail.com>
2017-06-12 19:34:35 -0500

    Replace bootstrap components with grommet in AccountGroups

--- src/account/AccountsGroup.jsx
+++ src/account/AccountsGroup.jsx
@@ -1,38 +1,56 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
-// import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
+import { Accordion, AccordionPanel, Anchor, CreditCardIcon, MoneyIcon, List, ListItem } from 'grommet';
 
-const AccountsGroup = ({ title, icon, accounts, activeAccountId }) => {
-  const panelHeader = (
+const AccountsGroup = ({ accounts, activeAccountId }) => {
+  const bankAccounts = accounts.filter(account => account.type === 'bank');
+  const ccAccounts = accounts.filter(account => account.type === 'creditcard');
+  const bankHeader = (
     <div>
-      <i className={'fa fa-lg fa-fw fa-' + icon }></i>
-      {` ${title}`}
+      <MoneyIcon />
+      Banks
+    </div>
+  );
+  const ccHeader = (
+    <div>
+      <CreditCardIcon />
+      Credit Cards
     </div>
   );
   return (
-    <Panel collapsible defaultExpanded header={panelHeader}>
-      <ListGroup fill>
-        {
-          accounts.map(function(account) {
-            const isActive = account._id === activeAccountId;
-            // disable clicking on currently displayed account in sidebar so that user
-            // can't change routes to the same route which resets uploadedTransactions state
-            return (
-              <Link to={`/account/${account._id}`} key={account._id}>
-                <ListGroupItem disabled={isActive}>{account.name}</ListGroupItem>
-              </Link>
-            );
-          })
-        }
-      </ListGroup>
-    </Panel>
+    <Accordion openMulti active={[0, 1]}>
+      <AccordionPanel heading={bankHeader}>
+        <List>
+          {
+            bankAccounts.map(function(account, i) {
+              // const isActive = account._id === activeAccountId;
+              // disable clicking on currently displayed account in sidebar so that user
+              // can't change routes to the same route which resets uploadedTransactions state
+              return (
+                <ListItem key={`bank${i}`}><Anchor path={`/account/${account._id}`} key={account._id}>{account.name}</Anchor></ListItem>
+              );
+            })
+          }
+        </List>
+      </AccordionPanel>
+      <AccordionPanel heading={ccHeader}>
+        <List>
+          {
+            ccAccounts.map(function(account, i) {
+              // const isActive = account._id === activeAccountId;
+              // disable clicking on currently displayed account in sidebar so that user
+              // can't change routes to the same route which resets uploadedTransactions state
+              return (
+                <ListItem key={`cc${i}`}><Anchor path={`/account/${account._id}`} key={account._id}>{account.name}</Anchor></ListItem>
+              );
+            })
+          }
+        </List>
+      </AccordionPanel>
+    </Accordion>
   );
 };
 AccountsGroup.propTypes = {
-  title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
   accounts: PropTypes.array.isRequired,
   activeAccountId: PropTypes.string,
 };
--- src/account/AccountsGroupContainer.jsx
+++ src/account/AccountsGroupContainer.jsx
@@ -5,13 +5,16 @@ import { bindActionCreators } from 'redux';
 import AccountsGroup from './AccountsGroup.jsx';
 import * as accountsActions from './AccountsActions.js';
 
-class AccountContainer extends Component {
+class AccountsGroupContainer extends Component {
   static propTypes = {
     accounts: PropTypes.array,
     actions: PropTypes.object.isRequired,
     activeAccountId: PropTypes.string,
   }
-
+  // ask for `router` from context
+  static contextTypes = {
+    router: React.PropTypes.object
+  }
   componentWillMount() {
     this.props.actions.getAccounts();
   }
@@ -20,19 +23,16 @@ class AccountContainer extends Component {
     if (!this.props.accounts) {
       return <div>Loading...</div>;
     }
-    const ccAccounts = this.props.accounts.filter(account => account.type === 'creditcard');
-    const bankAccounts = this.props.accounts.filter(account => account.type === 'bank');
     return (
-      <div>
-        <AccountsGroup title="Banks" icon="bank" accounts={bankAccounts} activeAccountId={this.props.activeAccountId} />
-        <AccountsGroup title="Credit Cards" icon="credit-card" accounts={ccAccounts} activeAccountId={this.props.activeAccountId} />
-      </div>
+      <AccountsGroup
+        accounts={this.props.accounts}
+        activeAccountId={this.props.activeAccountId} />
     );
   }
 }
 
 // Anything returned from this function will end up as props
-// on the AccountContainer container
+// on the AccountsGroupContainer container
 function mapStateToProps(state) {
   return {
     // state comes from the <Provider>'s 'store' property in index.js
@@ -47,9 +47,9 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-// Promote AccountContainer from a Component to a Container, a
+// Promote AccountsGroupContainer from a Component to a Container, a
 // component that is aware of the state that's contained by Redux
 // 'connect' takes a Function and a Component and produces a Container
 // arguments are passed into the Container as props(?)
 // https://github.com/rackt/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
-export default connect(mapStateToProps, mapDispatchToProps)(AccountContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(AccountsGroupContainer);
