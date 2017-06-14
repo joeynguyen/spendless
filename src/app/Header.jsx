5bf3396f3857af1d4fc555bcab69b00d5dfb22c4
Joey Nguyen <jnn193@gmail.com>
2017-06-14 00:47:46 -0500

    Fix Header elements alignment

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Anchor, Box, Header as GrommetHeader, Menu  } from 'grommet';
+import { Anchor, Box, Header as GrommetHeader, Menu, Title  } from 'grommet';
 
 const styles = {
+  logoAnchor: {
+    textDecoration: 'none',
+  },
   logoSpan: {
     fontFamily: ['Dawning of a New Day', 'cursive'],
     fontSize: 28,
@@ -12,21 +15,21 @@ const styles = {
 
 const Header = () => {
   return (
-    <Box pad="small" colorIndex="neutral-1">
-      <GrommetHeader>
-        <Link to="/">
+    <GrommetHeader colorIndex="neutral-1" pad="small" justify="between">
+      <Title>
+        <Link to="/" style={styles.logoAnchor}>
           <img src="../assets/piggybank.png" width="50px"/>
           {' '}
           <span style={styles.logoSpan}>spendLess</span>
         </Link>
-        <Box flex justify="center" direction="row" responsive={false}>
-          <Menu responsive inline direction="row" size="large">
-            <Anchor path="/" className="active">Overview</Anchor>
-            <Anchor path="/help">Help</Anchor>
-          </Menu>
-        </Box>
-      </GrommetHeader>
-    </Box>
+      </Title>
+      <Box align="center" direction="row">
+        <Menu direction="row" size="large" dropAlign={{right: 'right'}}>
+          <Anchor path="/" className="active">Overview</Anchor>
+          <Anchor path="/help">Help</Anchor>
+        </Menu>
+      </Box>
+    </GrommetHeader>
   );
 };
 
