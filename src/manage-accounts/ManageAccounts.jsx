8182c7f6f004ea4debcdb308e65ca77ee289176e
Joey Nguyen <jnn193@gmail.com>
2017-06-14 02:09:06 -0500

    Replace modal component in ManageAccounts

--- src/manage-accounts/ManageAccounts.jsx
+++ src/manage-accounts/ManageAccounts.jsx
@@ -1,42 +1,47 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Article, Box, Button, Heading, Layer, Section } from 'grommet';
 import AddAccount from './AddAccount.jsx';
 import AccountsList from './AccountsList.jsx';
 
 const ManageAccounts = ({ accounts, actions, manageAccountsVisible }) => {
   const ccAccounts = accounts.filter(account => account.type === 'creditcard');
   const bankAccounts = accounts.filter(account => account.type === 'bank');
-  return (
-    <Modal
-      show={manageAccountsVisible}
-      onHide={actions.toggleManageAccounts}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>Manage Accounts</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="row">
-          <div className="col-xs-8">
-            <AddAccount />
+  let modal = null;
+  if (manageAccountsVisible) {
+    modal = (
+      <Layer closer onClose={actions.toggleManageAccounts}>
+        <Article>
+          <Section pad={{vertical: 'medium'}}>
+            <Heading tag="h3">Manage Accounts</Heading>
             <hr />
-            <AccountsList accounts={accounts} />
-          </div>
+            <AddAccount />
+          </Section>
+          <Section direction="row" pad={{vertical: 'small'}}>
+            <Box size="medium">
+              <AccountsList accounts={accounts} />
+            </Box>
+            <Box size="small">
+              <ul>
+                <li>{bankAccounts.length} Bank accounts</li>
+                <li>{ccAccounts.length} Credit card accounts</li>
+              </ul>
+            </Box>
+          </Section>
+          <hr />
+          <Section align="end" pad={{vertical: 'medium'}}>
+            <Button
+              name="close-manage-accounts"
+              onClick={actions.toggleManageAccounts}
+              label="Close"
+            />
+          </Section>
+        </Article>
+      </Layer>
+    );
+  }
 
-          <div className="col-xs-4">
-            <ul>
-              <li>{bankAccounts.length} Bank accounts</li>
-              <li>{ccAccounts.length} Credit card accounts</li>
-            </ul>
-          </div>
-        </div>
-
-      </Modal.Body>
-      <Modal.Footer>
-        <Button name="close-manage-accounts" onClick={actions.toggleManageAccounts}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+  return modal;
 };
 ManageAccounts.propTypes = {
   accounts: PropTypes.array.isRequired,
