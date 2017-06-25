f89a1cba56b9084df5a121e506aacfefcd8a3564
Joey Nguyen <jnn193@gmail.com>
2017-06-24 20:55:22 -0500

    Replaced bootstrap input fields with ConnectedTextInput

--- src/account/ManageTransaction.jsx
+++ src/account/ManageTransaction.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Article, Box, Button, Form, Heading, Section } from 'grommet';
 
+import ConnectedTextInput from '../custom-components/ConnectedTextInput.jsx';
 import FieldGroup from '../custom-components/FieldGroup.jsx';
 
 const ManageTransaction = ({ manageType = 'add', fields, toggleManageTransaction, pristine = true, doSubmit }) => {
@@ -16,68 +17,67 @@ const ManageTransaction = ({ manageType = 'add', fields, toggleManageTransaction
       <hr />
       <Form onSubmit={doSubmit}>
         <Box pad="none" margin={{vertical: 'medium'}}>
-            <FieldGroup
-              type="text"
-              label="Description"
-              name={description.name}
-              error={description.error}
-              invalid={description.invalid}
-              touched={description.touched}
-              onBlur={description.onBlur}
-              onChange={description.onChange}
-              onFocus={description.onFocus}
-              value={description.value}
-            />
-            <FieldGroup
-              type="date"
-              label="Date"
-              name={date.name}
-              error={date.error}
-              invalid={date.invalid}
-              touched={date.touched}
-              onBlur={date.onBlur}
-              onChange={date.onChange}
-              onFocus={date.onFocus}
-              value={date.value}
-            />
-            <FieldGroup
-              type="text"
-              label="Category"
-              name={category.name}
-              error={category.error}
-              invalid={category.invalid}
-              touched={category.touched}
-              onBlur={category.onBlur}
-              onChange={category.onChange}
-              onFocus={category.onFocus}
-              value={category.value}
-            />
-            <FieldGroup
-              type="text"
-              addonBefore="$"
-              label="Amount"
-              name={amount.name}
-              error={amount.error}
-              invalid={amount.invalid}
-              touched={amount.touched}
-              onBlur={amount.onBlur}
-              onChange={amount.onChange}
-              onFocus={amount.onFocus}
-              value={amount.value}
-            />
-            <FieldGroup
-              componentClass="textarea"
-              label="Notes"
-              placeholder="Add notes for this transaction"
-              name={notes.name}
-              error={notes.error}
-              invalid={notes.invalid}
-              touched={notes.touched}
-              onBlur={notes.onBlur}
-              onChange={notes.onChange}
-              onFocus={notes.onFocus}
-              value={notes.value}
-            />
+          <ConnectedTextInput
+            id={description.name}
+            label="Description"
+            name={description.name}
+            error={description.error}
+            invalid={description.invalid}
+            touched={description.touched}
+            onBlur={description.onBlur}
+            onChange={description.onChange}
+            onFocus={description.onFocus}
+            value={description.value}
+          />
+          <FieldGroup
+            type="date"
+            label="Date"
+            name={date.name}
+            error={date.error}
+            invalid={date.invalid}
+            touched={date.touched}
+            onBlur={date.onBlur}
+            onChange={date.onChange}
+            onFocus={date.onFocus}
+            value={date.value}
+          />
+          <ConnectedTextInput
+            id={category.name}
+            label="Category"
+            name={category.name}
+            error={category.error}
+            invalid={category.invalid}
+            touched={category.touched}
+            onBlur={category.onBlur}
+            onChange={category.onChange}
+            onFocus={category.onFocus}
+            value={category.value}
+          />
+          <ConnectedTextInput
+            id={amount.name}
+            label="Amount"
+            name={amount.name}
+            error={amount.error}
+            invalid={amount.invalid}
+            touched={amount.touched}
+            onBlur={amount.onBlur}
+            onChange={amount.onChange}
+            onFocus={amount.onFocus}
+            value={amount.value}
+          />
+          <FieldGroup
+            componentClass="textarea"
+            label="Notes"
+            placeholder="Add notes for this transaction"
+            name={notes.name}
+            error={notes.error}
+            invalid={notes.invalid}
+            touched={notes.touched}
+            onBlur={notes.onBlur}
+            onChange={notes.onChange}
+            onFocus={notes.onFocus}
+            value={notes.value}
+          />
         </Box>
         <hr />
         <Box align="end" pad="none" margin={{top: 'medium'}}>
