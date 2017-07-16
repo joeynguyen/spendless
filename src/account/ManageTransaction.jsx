26570e3e27828d64506062bb588a06c9b0ccf06b
Joey Nguyen <jnn193@gmail.com>
2017-07-16 01:59:30 -0500

    Replace bootstrap and redux-form in ManageTransaction

--- src/account/ManageTransaction.jsx
+++ src/account/ManageTransaction.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button, Modal } from 'antd';
+import { Button, DatePicker, Form, Input, Modal } from 'antd';
 
-import FieldGroup from '../custom-components/FieldGroup.jsx';
+const FormItem = Form.Item;
+const { TextArea } = Input;
 
-const ManageTransaction = ({ manageType = 'add', fields, manageTransactionVisible, toggleManageTransaction, pristine = true, doSubmit }) => {
-  const { date, description, category, amount, notes } = fields;
+const ManageTransaction = ({ form, manageType = 'add', manageTransactionVisible, toggleManageTransaction, doSubmit }) => {
+  const { getFieldDecorator } = form;
   const componentTitle = (manageType === 'edit') ? 'Edit Transaction' : 'Add Transaction';
   const modalFooter = [(
     <Button
       key="confirm"
       type="primary"
-      disabled={pristine}
       htmlType="submit"
       form="manage-transaction"
     >Save</Button>
@@ -34,79 +34,57 @@ const ManageTransaction = ({ manageType = 'add', fields, manageTransactionVisibl
       title={componentTitle}
       footer={modalFooter}
     >
-      <form id="manage-transaction" onSubmit={doSubmit}>
-        <FieldGroup
-          type="text"
-          label="Description"
-          name={description.name}
-          error={description.error}
-          invalid={description.invalid}
-          touched={description.touched}
-          onBlur={description.onBlur}
-          onChange={description.onChange}
-          onFocus={description.onFocus}
-          value={description.value}
-        />
-        <FieldGroup
-          type="date"
-          label="Date"
-          name={date.name}
-          error={date.error}
-          invalid={date.invalid}
-          touched={date.touched}
-          onBlur={date.onBlur}
-          onChange={date.onChange}
-          onFocus={date.onFocus}
-          value={date.value}
-        />
-        <FieldGroup
-          type="text"
-          label="Category"
-          name={category.name}
-          error={category.error}
-          invalid={category.invalid}
-          touched={category.touched}
-          onBlur={category.onBlur}
-          onChange={category.onChange}
-          onFocus={category.onFocus}
-          value={category.value}
-        />
-        <FieldGroup
-          type="text"
-          addonBefore="$"
-          label="Amount"
-          name={amount.name}
-          error={amount.error}
-          invalid={amount.invalid}
-          touched={amount.touched}
-          onBlur={amount.onBlur}
-          onChange={amount.onChange}
-          onFocus={amount.onFocus}
-          value={amount.value}
-        />
-        <FieldGroup
-          componentClass="textarea"
-          label="Notes"
-          placeholder="Add notes for this transaction"
-          name={notes.name}
-          error={notes.error}
-          invalid={notes.invalid}
-          touched={notes.touched}
-          onBlur={notes.onBlur}
-          onChange={notes.onChange}
-          onFocus={notes.onFocus}
-          value={notes.value}
-        />
-      </form>
+      <Form id="manage-transaction" onSubmit={doSubmit} layout="vertical">
+        <FormItem label="Description">
+          {getFieldDecorator('description', {
+            rules: [{ required: true, message: 'Enter a description' }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+
+        <FormItem label="Date">
+          {getFieldDecorator('date', {
+            rules: [{ required: true, message: 'Enter a date' }],
+          })(
+            <DatePicker />
+          )}
+        </FormItem>
+
+        <FormItem label="Category">
+          {getFieldDecorator('category', {
+            rules: [{ required: true, message: 'Enter a category' }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+
+        <FormItem label="Amount">
+          {getFieldDecorator('amount', {
+            rules: [{ required: true, message: 'Enter an amount' }],
+          })(
+            <Input addonBefore="$" />
+          )}
+        </FormItem>
+
+        <FormItem label="Notes">
+          {getFieldDecorator('notes', {
+            rules: [{ required: true, message: 'Enter an amount' }],
+          })(
+            <TextArea rows={4} placeholder="Add notes for this transaction"/>
+          )}
+        </FormItem>
+      </Form>
     </Modal>
   );
 };
 ManageTransaction.propTypes = {
+  form: PropTypes.object.isRequired,
+  manageTransactionVisible: PropTypes.bool.isRequired,
   manageType: PropTypes.string.isRequired,
   toggleManageTransaction: PropTypes.func.isRequired,
-  fields: PropTypes.object.isRequired,
-  pristine: PropTypes.bool.isRequired,
   doSubmit: PropTypes.func.isRequired,
 };
 
-export default ManageTransaction;
+export default Form.create({})(ManageTransaction);
+
--- src/app/AppContainer.jsx
+++ src/app/AppContainer.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 import AppHeader  from './Header.jsx';
 import AppSidebar from './Sidebar.jsx';
 import ManageAccountsContainer from '../manage-accounts/ManageAccountsContainer.jsx';
@@ -16,22 +17,24 @@ class AppContainer extends Component {
   }
   render() {
     return (
-      <Layout style={{'height': '100%', 'width': '100%'}}>
-        <Header style={{ background: '#fff', lineHeight: '70px', height: '70px' }}>
-          <AppHeader currentRoute={this.context.router.route.location.pathname} />
-        </Header>
+      <LocaleProvider locale={enUS}>
+        <Layout style={{'height': '100%', 'width': '100%'}}>
+          <Header style={{ background: '#fff', lineHeight: '70px', height: '70px' }}>
+            <AppHeader currentRoute={this.context.router.route.location.pathname} />
+          </Header>
 
-        <Layout>
-          <Sider collapsible style={{ background: '#333' }}>
-            <AppSidebar />
-          </Sider>
-          <Content style={{ padding: 16 }}>
+          <Layout>
+            <Sider collapsible style={{ background: '#333' }}>
+              <AppSidebar />
+            </Sider>
+            <Content style={{ padding: 16 }}>
             {this.props.children}
           </Content>
         </Layout>
 
         <ManageAccountsContainer />
       </Layout>
+      </LocaleProvider>
     );
   }
 }
