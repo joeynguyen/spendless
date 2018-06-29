5838cd5699c2cb7e3298b2f7203e03a365d21eec
Joey Nguyen <jnn193@gmail.com>
2018-06-28 19:55:03 -0500

    Disable service worker because this is an app, not website

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { registerObserver } from 'react-perf-devtool';
 import Root from './app/Root.jsx';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { configureStore, history } from './store/configureStore.js';
 
 const store = configureStore();
@@ -17,4 +17,4 @@ ReactDOM.render(
   <Root store={store} history={history} />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+// registerServiceWorker();
