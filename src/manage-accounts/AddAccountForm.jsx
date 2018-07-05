cc0f6a6ae1561e9ef68f8072ba4641f13e990994
Joey Nguyen <jnn193@gmail.com>
2018-07-04 23:46:36 -0500

    Converted class component into functional component

--- src/manage-accounts/AddAccountForm.jsx
+++ src/manage-accounts/AddAccountForm.jsx
@@ -1,37 +1,35 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Radio } from 'antd';
 
 import AccountTypeFormContainer from './AccountTypeFormContainer.jsx';
 
 const FormItem = Form.Item;
 
-export class AddAccountForm extends Component {
-  render() {
-    const { getFieldDecorator } = this.props.form;
+const AddAccountForm = ({ form }) => {
+  const { getFieldDecorator } = form;
 
-    const accountTypeSelected = this.props.form.getFieldValue('accountType');
+  const accountTypeSelected = form.getFieldValue('accountType');
 
-    return (
-      <div className="well">
-        <Form layout="vertical">
-          <FormItem label="Type">
-            {getFieldDecorator('accountType', {
-              initialValue: 'bank',
-            })(
-              <Radio.Group>
-                <Radio.Button value="bank">Bank</Radio.Button>
-                <Radio.Button value="creditcard">Credit Card</Radio.Button>
-              </Radio.Group>
-            )}
-          </FormItem>
-        </Form>
+  return (
+    <div className="well">
+      <Form layout="vertical">
+        <FormItem label="Type">
+          {getFieldDecorator('accountType', {
+            initialValue: 'bank',
+          })(
+            <Radio.Group>
+              <Radio.Button value="bank">Bank</Radio.Button>
+              <Radio.Button value="creditcard">Credit Card</Radio.Button>
+            </Radio.Group>
+          )}
+        </FormItem>
+      </Form>
 
-        <AccountTypeFormContainer accountType={accountTypeSelected} />
-      </div>
-    );
-  }
-}
+      <AccountTypeFormContainer accountType={accountTypeSelected} />
+    </div>
+  );
+};
 
 AddAccountForm.propTypes = {
   form: PropTypes.object.isRequired,
